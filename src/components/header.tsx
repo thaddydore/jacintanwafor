4a37a404c38df84104180de3ed531cd6496c8f71
thaddydore <thaddydore@gmail.com>
2024-06-13 20:50:29 +0100

    fix flow-bite production issue

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,46 +8,44 @@ interface Props {
 const Header = ({ color }: Props) => {
 	const location = useLocation();
 	return (
-		<>
-			<Navbar
-				fluid
-				rounded
-				className={`${
-					color === 'black' ? 'bg-[#141617] text-[#C2C8CB]' : 'bg-white text-[#061818]'
-				} py-4 lg:px-[300px] px-6`}
-			>
-				<Navbar.Brand as={Link} to='/'>
-					<span className='self-center whitespace-nowrap text-xl font-semibold dark:text-white'>
-						Jacinta Chianumba
-					</span>
-				</Navbar.Brand>
-				<Navbar.Toggle />
-				<Navbar.Collapse>
-					<Navbar.Link
-						to='/'
-						as={Link}
-						className={`${location.pathname === '/' ? 'underline decoration-2 decoration-[#73A3A9]' : ''}`}
-					>
-						Work
-					</Navbar.Link>
-					<Navbar.Link
-						as={Link}
-						to='/about'
-						className={`${location.pathname === '/about' ? 'underline decoration-2 decoration-[#73A3A9]' : ''}`}
-					>
-						About
-					</Navbar.Link>
-					<Navbar.Link
-						as={Link}
-						to='https://drive.google.com/file/d/14OvCGU2F2y7YWg7OsyTW-ZzZkI7k8i6X/view?usp=sharing'
-						target='_blank'
-						className={`${location.pathname === '/resume' ? 'underline decoration-2 decoration-[#73A3A9]' : ''}`}
-					>
-						Resume
-					</Navbar.Link>
-				</Navbar.Collapse>
-			</Navbar>
-		</>
+		<Navbar
+			fluid
+			rounded
+			className={`${
+				color === 'black' ? 'bg-[#141617] text-[#C2C8CB]' : 'bg-white text-[#061818]'
+			} py-4 lg:px-[300px] px-6`}
+		>
+			<Navbar.Brand as={Link} to='/'>
+				<span className='self-center whitespace-nowrap text-xl font-semibold dark:text-white'>
+					Jacinta Chianumba
+				</span>
+			</Navbar.Brand>
+			<Navbar.Toggle />
+			<Navbar.Collapse>
+				<Navbar.Link
+					to='/'
+					as={Link}
+					className={`${location.pathname === '/' ? 'underline decoration-2 decoration-[#73A3A9]' : ''}`}
+				>
+					Work
+				</Navbar.Link>
+				<Navbar.Link
+					as={Link}
+					to='/about'
+					className={`${location.pathname === '/about' ? 'underline decoration-2 decoration-[#73A3A9]' : ''}`}
+				>
+					About
+				</Navbar.Link>
+				<Navbar.Link
+					as={Link}
+					to='https://drive.google.com/file/d/14OvCGU2F2y7YWg7OsyTW-ZzZkI7k8i6X/view?usp=sharing'
+					target='_blank'
+					className={`${location.pathname === '/resume' ? 'underline decoration-2 decoration-[#73A3A9]' : ''}`}
+				>
+					Resume
+				</Navbar.Link>
+			</Navbar.Collapse>
+		</Navbar>
 	);
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
+const flowbite = require('flowbite-react/tailwind');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [  "./src/**/*.{js,jsx,ts,tsx}",  'node_modules/flowbite-react/lib/esm/**/*.js'],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('flowbite/plugin')
-]
-
-
-}
-
-
+	content: ['./src/**/*.{js,jsx,ts,tsx}', flowbite.content()],
+	theme: {
+		extend: {},
+	},
+	plugins: [flowbite.plugin()],
+};
